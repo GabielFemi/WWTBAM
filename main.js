f9cccdabfe530ebfdec3f33918b0c165863b49b0
GabielFemi <gabrielfemi799@gmail.com>
2020-06-30 14:34:55 +0100

    Append question and answer to main-div

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $.getJSON("quiz.json", function( data ) {
 let items = [];
 $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+    items.push( "<li id='" + key + "' class='" + key + "'>" + val + "</li>" );
 });
 
 $( "<ul/>", {
     "class": "my-new-list",
     html: items.join( "" )
-}).appendTo( "body" );
+}).appendTo( ".main-div" );
 });
